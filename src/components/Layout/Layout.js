11d67c580280c6f2235026f13e3fa80a608dd5ab
Erick Rosete Beas <erickrosetebeas@hotmail.com>
2019-05-06 12:57:12 -0700

    changed whatsapp button and number

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -8,9 +8,9 @@ import "./Layout.css";
 import Button from 'react-bootstrap/Button'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const whatsUrl="https://wa.me/15551234567?text=Iam%20interested%20"
+const whatsUrl = "https://wa.me/5215544433759?text=Me%20interesa%20"
 // const whatsUrlButton=`javascript:window.open('${whatsUrl}', 'yourWindowName', 'width=700,height=600,top=200, left=460');`
-const displayWhats=()=>{
+const displayWhats = () => {
   window.open(whatsUrl, 'yourWindowName', 'width=700,height=600,top=200, left=460')
 }
 
@@ -23,7 +23,7 @@ const Layout = props => {
       {/* <a href="https://wa.me/15551234567?text=I'm%20interested%20in%20your%20car%20for%20sale">contact</a> */}
       {/* <a href={whatsUrlButton}>Test</a> */}
       <Button variant="success" className="contactWhats" onClick={displayWhats}>
-          WhatsApp <FontAwesomeIcon icon={['fab', 'whatsapp']}  size="1x"/>
+        <FontAwesomeIcon icon={['fab', 'whatsapp']} size='2x' />
       </Button>
       {/* <FontAwesomeIcon className="marcadormagico"  icon="whatsapp-square" size="5x" /> */}
 
